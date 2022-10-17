410e63ae95f0ba1719b2ade3fa63a9c8dda396da
kyrosle <le@90e.com>
2022-10-17 11:04:55 +0800

    add web camera

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,24 @@
-use dioxus::prelude::{Component, Element, UseRef, VNode};
+use dioxus::prelude::UseRef;
 use gloo_utils::format::JsValueSerdeExt;
 use tracing::info;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{MediaStream, MediaStreamConstraints};
+use web_sys::{MediaStream, MediaStreamConstraints, HtmlVideoElement};
 pub struct VideoStream {
-    el: Component,
+    el: HtmlVideoElement,
 }
 
 impl VideoStream {
-    pub fn new(el: Component) -> VideoStream {
+    pub fn new(el: HtmlVideoElement) -> VideoStream {
         VideoStream { el }
     }
 
-    pub async fn set_video_src(&self, video_constraints: &serde_json::Value) {
+    pub async fn set_video_src(
+        &self,
+        video_constraints: &serde_json::Value,
+        stream: &mut UseRef<Option<MediaStream>>,
+    ) {
+        // get media device by js binding
         let window = web_sys::window().expect("no global `window` exists");
         let navigator = window.navigator();
         let devices = navigator
@@ -25,6 +30,7 @@ impl VideoStream {
         let mut constraints = MediaStreamConstraints::new();
         constraints.video(&JsValue::from_serde(video_constraints).unwrap());
         constraints.audio(&false.into());
+
         let media = JsFuture::from(
             devices
                 .get_user_media_with_constraints(&constraints)
@@ -33,7 +39,8 @@ impl VideoStream {
         .await
         .unwrap();
         let media_stream = media.unchecked_into::<MediaStream>();
-        // let media_stream = media.dyn_into();
         info!("media stream(tracing_wasm): {:?}", media_stream);
+        self.el.set_src_object(Some(&media_stream));
+        stream.write().replace(media_stream);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use dioxus::prelude::*;
 use serde_json::json;
 use t_dix::VideoStream;
-use wasm_bindgen::JsValue;
-use web_sys::MediaStreamConstraints;
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{HtmlMediaElement, HtmlVideoElement, MediaStream, MediaStreamConstraints};
 
 fn main() {
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
-    dioxus::web::launch(videoNode);
+    dioxus::web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
@@ -20,65 +21,74 @@ fn app(cx: Scope) -> Element {
         }
     })
 }
-fn videoNode(cx: Scope) -> Element {
-    let stream = use_ref(&cx, || None);
+// #[allow(non_snake_case)]
+// fn videoNode(cx: Scope) -> Element {
+//     let stream = use_ref(&cx, || None);
 
-    // when the element is mounted, bind the video element to the scope
-    use_future(&cx, move || {
-        let stream = stream.clone();
-        async move {
-            let md = gloo_utils::window().navigator().media_devices().unwrap();
-            let mut constraints = MediaStreamConstraints::new();
-            constraints.video(&JsValue::from(true));
-            constraints.audio(&JsValue::from(true));
-            let gum = md.get_user_media_with_constraints(&constraints).unwrap();
-            let cam_video = JsFuture::from(gum).await.map(MediaStream::from);
+//     // when the element is mounted, bind the video element to the scope
+//     let _ = use_future(&cx, (), |_| {
+//         let stream = stream.clone();
+//         async move {
+//             let md = gloo_utils::window().navigator().media_devices().unwrap();
+//             let mut constraints = MediaStreamConstraints::new();
+//             constraints.video(&JsValue::from(true));
+//             constraints.audio(&JsValue::from(true));
+//             let gum = md.get_user_media_with_constraints(&constraints).unwrap();
+//             let cam_video = JsFuture::from(gum).await.map(MediaStream::from);
 
-            if let Ok(ms) = cam_video {
-                gloo_utils::document()
-                    .get_element_by_id("my-video")
-                    .unwrap()
-                    .dyn_into::<HtmlMediaElement>()
-                    .unwrap()
-                    .set_src_object(Some(&ms));
-                stream.write().replace(ms);
-            }
-        }
-    });
+//             if let Ok(ms) = cam_video {
+//                 gloo_utils::document()
+//                     .get_element_by_id("my-video")
+//                     .unwrap()
+//                     .dyn_into::<HtmlMediaElement>()
+//                     .unwrap()
+//                     .set_src_object(Some(&ms));
+//                 stream.write().replace(ms);
+//             }
+//         }
+//     });
 
-    cx.render(rsx! {
-        video { id: "my-video", auto_play: "true" /* autoplay is very important */ }
-    })
-}
+//     cx.render(rsx! {
+//         video {
+//             id: "my-video",
+//             autoplay: "true",
+//         }
+//     })
+// }
 
 #[allow(non_snake_case)]
 fn Video(cx: Scope) -> Element {
-    let video_node: Component = |cx| {
-        cx.render(rsx! {
-            video {
-            id: "my-video",
-            class: "border border-gray-400 rounded-lg",
-            autoplay: "true",
-            width: "1280",
-            height: "720",
-        }})
-    };
-    let video_ref = use_ref(&cx, || video_node);
-    let _ = use_future(&cx, (), |_| async move {
-        let video = VideoStream::new(video_node);
-        video
-            .set_video_src(&json!({
-                "audio": false,
-                "video": {
-                    "facingMode": "environment",
-                    "width": 640,
-                    "height": 480,
-                }
-            }))
-            .await;
+    let stream = use_ref(&cx, || None);
+
+    let _ = use_future(&cx, (), |_| {
+        let mut stream = stream.clone();
+        let video_constraints = json!({
+            "audio": false,
+            "video": {
+                "facingMode": "environment",
+                "width": 640,
+                "height": 480,
+            }
+        });
+        async move {
+            let el = gloo_utils::document()
+                .get_element_by_id("my-video")
+                .unwrap()
+                .dyn_into::<HtmlVideoElement>()
+                .unwrap();
+            let video_stream = VideoStream::new(el);
+            video_stream.set_video_src(&video_constraints, &mut stream).await;
+        }
     });
     cx.render(rsx! {
         div {
+            video {
+                id: "my-video",
+                class: "border border-gray-400 rounded-lg",
+                autoplay: "true",
+                width: "640",
+                height: "480",
+            }
         }
     })
 }
